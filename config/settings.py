ce1dfbbc8b727046be0eabfe8c6ca13a1bef22c6
김훈희 <nmdkims@gmail.com>
2022-09-08 01:36:24 +0900

    Add: #2 user 앱 추가
    1. user 앱 추가
    2. setting.py INSTALLED_APPS 설정 추가

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -40,6 +40,16 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
+
+PROJECT_APPS = [
+    "user",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
