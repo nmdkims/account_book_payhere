9dad09b2aa39f245bf32a323b0516b0f00cc8290
김훈희 <nmdkims@gmail.com>
2022-09-22 11:22:00 +0900

    Refactor : Docker 관련 설정 수정

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
+
 from datetime import timedelta
 from pathlib import Path
 
@@ -128,11 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # 9/22
-# STATIC_URL = "static/"
-# STATIC_ROOT = ".static/"
+STATIC_URL = "static/"
+STATIC_ROOT = ".static/"
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_URL = "/static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
